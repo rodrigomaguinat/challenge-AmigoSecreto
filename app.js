8d701201083cdb6029fdc8a37b002a024b65ae77
rodrigomaguinat <rodrigo.maguina.t@uni.pe>
2025-09-02 08:48:33 -0500

    function agregarAmigo()

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+
+function agregarAmigo(){
+    let element = document.getElementById('amigo').value;
+
+    if (element == ""){
+        alert("Por favor, inserte un nombre.");
+    }else{
+        amigos.push(element);
+    }
+    limpiarCampo();
+    //console.log(amigos);
+}
+
+function limpiarCampo() {
+  const campo = document.getElementById('amigo');
+  campo.value = ''; // Limpia el campo de texto
+}
